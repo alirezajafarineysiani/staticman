5d028d710aaf4435009e206922c07da7dca7419b
Alireza Jafari-Neysiani <alirezajafarineysiani@yahoo.com>
2025-08-18 00:04:42 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 try {
-  const StaticmanAPI = require('./server')
-  const api = new StaticmanAPI()
+  console.log('ENV check:',
+    'NODE_ENV=', process.env.NODE_ENV,
+    'has GITHUB_TOKEN=', !!process.env.GITHUB_TOKEN,
+    'has RSA_PRIVATE_KEY=', !!process.env.RSA_PRIVATE_KEY
+  );
+
+  const StaticmanAPI = require('./server');
+  const api = new StaticmanAPI();
 
   api.start(port => {
-    console.log('Staticman API running on port', port)
-  })
+    console.log('Staticman API running on port', port);
+  });
 } catch (e) {
-  console.error(e)
+  console.error(e);
 }
